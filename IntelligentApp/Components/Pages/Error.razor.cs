3af043778e82cb77dc4b546bec8baaccd352f1c6
Michal L. <26222619+dotnet-programmer@users.noreply.github.com>
2025-07-03 14:25:15 +0200

    Preparing a Blazor project

---
+++ IntelligentApp/Components/Pages/Error.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Components;
+
+namespace IntelligentApp.Components.Pages;
+
+public partial class Error
+{
+	[CascadingParameter]
+	private HttpContext? HttpContext { get; set; }
+
+	private string? RequestId { get; set; }
+
+	private bool ShowRequestId 
+		=> !string.IsNullOrEmpty(RequestId);
+
+	protected override void OnInitialized() 
+		=> RequestId = Activity.Current?.Id ?? HttpContext?.TraceIdentifier;
+}
---
+++ IntelligentApp/Components/Pages/HelloUser.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace IntelligentApp.Components.Pages;
+
+public partial class HelloUser
+{
+	private readonly string _name = "Michał";
+
+	private string GetGreeting()
+		=> $"Hello, {_name}!";
+}
