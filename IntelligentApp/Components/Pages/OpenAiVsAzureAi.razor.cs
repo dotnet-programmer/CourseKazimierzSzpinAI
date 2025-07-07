348ac583410ef3cb37ae69df82d0a568c9a70c37
Michal L. <26222619+dotnet-programmer@users.noreply.github.com>
2025-07-07 15:35:48 +0200

    Homework 2

---
+++ IntelligentApp/Components/Pages/OpenAiVsAzureAi.razor.cs
@@ -0,0 +1,41 @@
+﻿using IntelligentApp.HttpRepository.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace IntelligentApp.Components.Pages;
+
+public partial class OpenAiVsAzureAi
+{
+	private List<string> _keyPhrases = [];
+	private string _baseText = string.Empty;
+	private string _generatedText = string.Empty;
+	private bool _isLoading = false;
+
+	[Inject]
+	public IOpenAiHttpRepository OpenAiHttpRepository { get; set; }
+
+	[Inject]
+	public IAzureAiHttpRepository AzureAiHttpRepository { get; set; }
+
+	private async Task ProcessTextAsync()
+	{
+		_isLoading = true;
+
+		_generatedText = await OpenAiHttpRepository.AskOpenAiAsync($"Przeczytaj poniższy tekst i wygeneruj krótkie streszczenie:\n\n{_baseText}.");
+		_keyPhrases = await AzureAiHttpRepository.ExtractKeyPhrasesAsync(_baseText);
+
+		_isLoading = false;
+	}
+
+	private MarkupString HighlightKeyPhrases(string text, List<string> phrases)
+	{
+		var highlighted = text;
+		foreach (var phrase in phrases)
+		{
+			if (!string.IsNullOrWhiteSpace(phrase))
+			{
+				highlighted = highlighted.Replace(phrase, $"<span style=\"background-color:yellow\">{phrase}</span>", StringComparison.OrdinalIgnoreCase);
+			}
+		}
+		return (MarkupString)highlighted;
+	}
+}
