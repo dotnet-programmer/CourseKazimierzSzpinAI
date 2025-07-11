1b5dc980042c77c259e90288c0a320864336f91f
Michal L. <26222619+dotnet-programmer@users.noreply.github.com>
2025-07-11 10:28:45 +0200

    Content Assistant - generating name and description

--- IntelligentApp/Components/Pages/ContentAssistant/ContentAssistant.razor.cs
+++ IntelligentApp/Components/Pages/ContentAssistant/ContentAssistant.razor.cs
@@ -4,11 +4,14 @@ using Microsoft.JSInterop;
 
 namespace IntelligentApp.Components.Pages.ContentAssistant;
 
-public partial class ContentAssistant(IJSRuntime JS, IFileService fileService, IAzureSpeechHttpRepository azureSpeech)
+public partial class ContentAssistant(IJSRuntime JS, IFileService fileService, IAzureSpeechHttpRepository azureSpeech, IOpenAiHttpRepository openAi)
 {
+	private bool _isLoading = false;
 	private bool _isRecording = false;
 	private string? _userProductDesc;
 	private byte[]? _audio;
+	private string? _generatedName;
+	private string? _generatedDescription;
 
 	private async Task StartRecordingAsync()
 	{
@@ -35,5 +38,42 @@ public partial class ContentAssistant(IJSRuntime JS, IFileService fileService, I
 		var wavFile = await fileService.ConvertWebmToWavAsync(_audio);
 		_userProductDesc = await azureSpeech.GetTextAsync(wavFile);
 		_isRecording = false;
+	}
+
+	private async Task GenerateAsync()
+	{
+		if (string.IsNullOrWhiteSpace(_userProductDesc))
+		{
+			return;
+		}
+
+		_isLoading = true;
+
+		await GenerateNameAsync();
+		await GenerateDescriptionAsync();
+
+		_isLoading = false;
+	}
+
+	private async Task GenerateNameAsync()
+	{
+		var prompt = $@"
+Wygeneruj krótką, chwytliwą nazwę produktu na podstawie poniższych informacji:
+{_userProductDesc}
+Użyj języka polskiego. Podaj tylko samą nazwę, bez dodatkowego wyjaśnienia.";
+
+		_generatedName = await openAi.AskOpenAiAsync(prompt);
+	}
+
+	private async Task GenerateDescriptionAsync()
+	{
+		var prompt = $@"
+Na podstawie tych informacji o produkcie:
+{_userProductDesc}
+Stwórz angażujący, marketingowy opis produktu w języku polskim.
+Napisz w taki sposób, by był atrakcyjny dla potencjalnego klienta.
+Opis ma zawierać około 100 znaków.";
+
+		_generatedDescription = await openAi.AskOpenAiAsync(prompt);
 	}
 }
